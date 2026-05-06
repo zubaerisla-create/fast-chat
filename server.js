638753b03459d3638006b0a2324f7892a02e5c43
abdullahalzubaerofficial <zubaer.steadfast@gmail.com>
2026-05-07 03:11:14 +0600

    updated

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ connectDB();
 // ─── Core Middleware ──────────────────────────────────────────────────────────
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || ["http://localhost:3000", "http://localhost:3001", "https://fast-chat-frontend-swart.vercel.app"],
+    origin: process.env.CLIENT_URL || ["http://localhost:3000", "http://localhost:3001", "https://fast-chat-frontend-swart.vercel.app","CLIENT_URL=https://fast-chat-frontend-app.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -58,7 +58,7 @@ const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.CLIENT_URL || ["http://localhost:3000", "http://localhost:3001" , "https://fast-chat-frontend-swart.vercel.app"],
+    origin: process.env.CLIENT_URL || ["http://localhost:3000", "http://localhost:3001" , "https://fast-chat-frontend-swart.vercel.app","CLIENT_URL=https://fast-chat-frontend-app.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
